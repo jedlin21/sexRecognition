1c3d0b1b9916ebaf784099e6f02e7bdf0fbb30b9
Patryk Jedlikowski <patryk.jedlikowski@sybillatechnologies.com>
2019-12-30 18:30:42 +0100

    read wav

---
+++ sexRecognition.py
@@ -0,0 +1,18 @@
+from scipy.io import wavfile
+import glob
+
+
+
+
+def main():
+    regex = "trainall/*.wav"
+    files = glob.glob(f"./{regex}", recursive=True)
+    for f in files:
+        print(f)
+        fs, data = wavfile.read(f)
+        print(fs)
+        print(data)
+        break
+
+
+main()
