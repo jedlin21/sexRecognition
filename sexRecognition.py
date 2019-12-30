d523b79636629f80ec513b16f7af6ebc8e56ee6a
Patryk Jedlikowski <patryk.jedlikowski@sybillatechnologies.com>
2019-12-30 20:00:06 +0100

    signal.decimate (???)

--- sexRecognition.py
+++ sexRecognition.py
@@ -13,18 +13,13 @@ def main():
     for f in files:
         print(f)
         fs, data = wavfile.read(f)
-        print(f"shape {data.shape}")
         if len(data.shape) != 1:
             data = data[:, 0]
-        print(len(data))
         data = data[ int(len(data)*0.1) : int(len(data)*0.9) ] #cut ends of the signal to avoid interference
-        print(len(data))
-        print(fs)
-        print(data)
-
         plt.plot(data)
         plt.show()
 
+        # FFT
         signal1 = fft(data)
         signal1 = abs(signal1) / len(data) * 2 
         signal1[0] = 0
@@ -34,9 +29,10 @@ def main():
         plt.plot(freqs[:10000], signal1[:10000])
         plt.xlabel("Częstotliwość")
         plt.ylabel("Amplituda")
-        #plt.show()
+        plt.show()
 
         ################
+        # keiser
         N = 10000
         data = signal1[:N]
         freqs = freqs[:N]
@@ -46,11 +42,20 @@ def main():
         plt.plot(freqs, signal_kaiser)
         plt.show()
 
-        maxIndex = np.argmax(signal_kaiser)
 
-        print(len(signal1))
-        print(len(freqs))
-        print(f"Max amplitude at: {freqs[maxIndex]}")
+        # signal.decimate
+        kopia_fft = signal_kaiser.copy()
+        for k in range(2,6):
+            d = signal.decimate(data, int(k))
+            kopia_fft[:len(d)] *= d
+
+        plt.plot(freqs, kopia_fft)
+        plt.show()
+
+        maxIndex = np.argmax(kopia_fft)
+        topFreq = freqs[maxIndex]
+        print(f"Max amplitude at: {topFreq}")
+        print(f"Extimated sex: {'M' if topFreq < 170 else 'K' }")
 
         
 
